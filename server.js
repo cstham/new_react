390ad0af4c11b683ba32c85d6a4edf2cc2f5fe47
cstham <cstha2@gmail.com>
2018-07-29 03:32:56 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+//STEPS
+//1) npm init -y (y represents answer yes to every prompt and this line...
+//   to generate package.json file.
+
+//2) npm install nodemon
+//3) npm install express
+//4) use "npm start" to start nodemon (realtime live changes)
+
+
+const express = require('express');
+const app = express();
+app.get('/', function(req, res){
+    res.send('hello world!');
+});
+app.listen(3000);
