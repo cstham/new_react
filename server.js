506855b23f7a0ccf0d6aa9b9eef7c017c7c6b220
cstham <cstha2@gmail.com>
2018-07-30 02:32:51 +0800

    changed folder name to instructions

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 //STEPS
 //1) npm init -y (y represents answer yes to every prompt and this line...
 //   to generate package.json file.
-
 //2) npm install nodemon
 //3) npm install express
-//4) npm install body-parser
-//5) use "npm start" to start nodemon (realtime live changes)
+//4) npm install body-parser (to parse json with express)
+//5) npm install bcrypt-nodejs (to hash passwords)
+//6) npm install cors (to connect front-end to back-end)
+//7) npm install knex (database)
+//8) use "npm start" to start nodemon (realtime live changes)
 
+
+//BACKEND INSTALLATION
+//1) install postman (to test server connections)
+//2) install postgresql relational database
 
 const express = require('express');
 const app = express();
